35d63be2c12788810a5bf713580e9603d848b0b5
nasiwebsite <31595474+nasiwebsite@users.noreply.github.com>
2018-12-25 03:07:13 +0700

    Update embed.php

--- embed.php
+++ embed.php
@@ -17,7 +17,7 @@ if($_GET['url'] != ""){
   <meta charset="utf-8" />
 	<title><?php echo $title?> - Google Drive</title>
 	<style type='text/css'>
-        $myElement {
+        #myElement {
           height: 100%;
           margin: 0;
           overflow: hidden;
